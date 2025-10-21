90d0e20fe64aca4c97549376905914a8c57bf404
Anderson F. Silva <andersonfabio.1976@gmail.com>
2025-10-21 11:26:30 -0300

    WIP - Merge Files

---
+++ backend/application/src/main/java/br/com/unifor/application/port/repository/MatriculaRepositoryPort.java
@@ -0,0 +1,5 @@
+package br.com.unifor.application.port.repository;
+
+import br.com.unifor.domain.model.Matricula;
+
+public interface MatriculaRepositoryPort extends BaseRepositoryPort<Matricula, Long> { }
